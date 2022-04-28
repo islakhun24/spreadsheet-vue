23a050e9d7221bf3e3f79df410c98b1b1aed1285
islakhun <islakhunnurdzaki22.magang@doku.com>
2022-04-28 15:14:17 +0700

    asdasd

--- src/App.vue
+++ src/App.vue
@@ -31,10 +31,19 @@ export default {
   name: 'App',
   data: () => ({
     spreadsheet: null,
-    options: {}
+    options: {},
   }),
   mounted: function () {
- this.options = {
+ 
+
+    fetch('https://0f6c-36-73-34-68.ap.ngrok.io/api/v1/order/to-sheets')
+      .then(response => response.json())
+      .then(json => {
+        return json.data;
+      }).then(json => {
+        // spreadsheet.setData(json)
+        console.log(json);
+        this.options = {
     data: [
       [''],
       ['']
@@ -44,27 +53,22 @@ export default {
     allowToolbar:true,
     columns: [
       { type: 'calendar', title: 'Tanggal Order', width: '250px' },
-      { type: 'dropdown', title: 'Kirim dari', width: '120px',  source: [ 'Pusdiklat IT', 'Purbalingga'] },
+      { type: 'dropdown', title: 'Kirim dari', width: '120px',  source: json.addresses },
       { type: 'text', title: 'Nama Pembeli', width: '250px'},
       { type: 'numeric', title: 'Nomor HP', width: '100px'},
       { type: 'numeric', title: 'Code Pos', width: '100px'},
       { type: 'text', title: 'Alamat Detail', width: '300px'},
-      { type: 'dropdown', title: 'Product', width: '120px',  source: [ 'Baju', 'Celana'] },
-      { type: 'dropdown', title: 'Variasi', width: '120px',  source: [ 'Baju - waran-hitam - ukuran L', 'Baju - waran-merah - ukuran XL'] },
+      { type: 'dropdown', title: 'Product', width: '120px',  source: json.products},
+      { type: 'dropdown', title: 'Variasi', width: '120px',  source: json.variant },
       { type: 'numeric', title: 'Kuantitas', width: '60px' },
-      { type: 'dropdown', title: 'Metode Pembayaran', width: '120px',  source: [ 'COD', 'BANK TRANSFER'] },
-      { type: 'dropdown', title: 'Expedisi', width: '120px',  source: [ 'JNE', 'Si Cepat'] },
+      { type: 'dropdown', title: 'Metode Pembayaran', width: '120px',  source: json.payment_method },
+      { type: 'dropdown', title: 'Expedisi', width: '120px',  source: json.shipments},
       { type: 'numeric', title: 'Nilai Pembayaran', width: '150px', mask: 'Rp #.###,00', decimal: ',' }
     ]
   }
 
     let spreadsheet = jexcel(this.$el, this.options)
     Object.assign(this, { spreadsheet })
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => {
-        spreadsheet.setData(json)
       })
   }
 }
